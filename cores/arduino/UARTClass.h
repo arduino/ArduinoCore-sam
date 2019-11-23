8b142997de5530acb787164c1e243ad6f814435a
Georg Icking-Konert <icking@onlinehome.de>
2019-11-23 14:14:58 +0100

    updated modification date
    - updated modification date to sync with SAM core
    - no functional change

--- cores/arduino/UARTClass.h
+++ cores/arduino/UARTClass.h
@@ -14,6 +14,8 @@
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Modified 23 November 2019 by Georg Icking-Konert
 */
 
 #ifndef _UART_CLASS_
